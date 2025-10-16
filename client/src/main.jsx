64312bfca6ad535e220e04474ae4b517cf20825f
Wallstreet889 <wallstreetcolumbia45@gmail.com>
2025-10-16 13:14:01 +0300

    4th edit update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeProvider } from './context/ThemeContext';
-import './styles/index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
